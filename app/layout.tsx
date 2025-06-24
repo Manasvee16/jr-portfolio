04d279bd9c165f742cab43e5da9230bbba0dbd78
Manasvee Rathie <133605330+Manasvee16@users.noreply.github.com>
2025-06-25 03:34:15 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Manasavee Rathie',
+  title: 'Manasvee Rathie',
   description: 'Portfolio Website',
   generator: 'Manasvee',
 }
